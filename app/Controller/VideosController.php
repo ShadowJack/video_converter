b9446b8a081c1bb997c51c11986dc1de5fa435be
shadowjack <kostyap@hotmail.ru>
2014-09-23 15:00:27 +0400

    version with php-ffmpeg library + info about video and audio added

--- app/Controller/VideosController.php
+++ app/Controller/VideosController.php
@@ -27,8 +27,42 @@ class VideosController extends AppController {
           //$path = $this->request->data['Video']['FLV_file']['tmp_name'];
           $path = WWW_ROOT."videos/".($this->Video->id).".flv";
           $out_path = WWW_ROOT."videos/".($this->Video->id).".mp4";
-          $this->VideoEncoder->convert_video($path, $out_path, 480, 360, true);
-          //$this->VideoEncoder->set_buffering($out_path);
+          
+          $ffprobe = FFMpeg\FFProbe::create(array('ffprobe.binaries' => '/usr/local/Cellar/ffmpeg/2.3.3/bin/ffprobe'));
+          //CakeLog::debug(print_r($ffprobe->streams($path)->videos()->first()->keys(), true));
+          $video_data = $ffprobe->streams($path)->videos()->first();
+          $width = $video_data->get('width');
+          $height = $video_data->get('height');
+          $this->Video->set(array('dimensions' => $width."x".$height));
+          $video_bit_rate = $video_data->get('bit_rate');
+          $this->Video->set(array('bv' => $video_bit_rate));
+          CakeLog::debug(print_r($video_bit_rate, true));
+          $audio_data = $ffprobe->streams($path)->audios()->first();
+          $audio_bit_rate = $audio_data->get('bit_rate');
+          $this->Video->set(array('ba' => $audio_bit_rate));
+          //CakeLog::debug(print_r($audio_data, true));
+          $ffmpeg = FFMpeg\FFMpeg::create(array(
+              'ffmpeg.binaries' => '/usr/local/Cellar/ffmpeg/2.3.3/bin/ffmpeg', //TODO: in production add binaries to PATH
+              'ffprobe.binaries' => '/usr/local/Cellar/ffmpeg/2.3.3/bin/ffprobe',
+              'timeout'          => 3600, // The timeout for the underlying process
+              'ffmpeg.threads'   => 5  // The number of threads that FFMpeg should use
+          ));
+          $video = $ffmpeg->open($path);
+          $format = new FFMpeg\Format\Video\X264();
+          $format->on('progress', function ($video, $format, $percentage) {
+              echo "$percentage % transcoded";
+          });
+
+          $format
+              -> setKiloBitrate($video_bit_rate/1000)
+              -> setAudioChannels(2)
+              -> setAudioKiloBitrate($audio_bit_rate/1000);
+          
+          $video
+              ->save($format, $out_path);
+          // save info about converted video into db
+          $this->Video->set(array('MP4' => "/videos/".($this->Video->id).".mp4"));
+          $this->Video->save();
           
           $this->Session->setFlash(__('Your video has been saved.'));
           return $this->redirect(array('action' => 'index'));
--- app/View/Videos/show.ctp
+++ app/View/Videos/show.ctp
@@ -1,13 +1,16 @@
-<h1><?php echo h($video['Video']['title']); ?></h1>
+<h1><strong><?php echo h($video['Video']['title']); ?></strong></h1>
 
 <p><small>Created: <?php echo $video['Video']['created']; ?></small></p>
-<p><?php echo $this->Html->link('FLV', $video['Video']['FLV'] ); ?></p>
-<p><?php
+<p>Download: <?php echo $this->Html->link('FLV', $video['Video']['FLV'] ); ?></p>
+<p>Download: <?php
   if ($video['Video']['MP4']) {
     echo $this->Html->link('MP4', $video['Video']['MP4'] );
   }
   else {
     echo('MP4 is not ready yet');
   } ?></p>
+<p>Dimensions: <?php echo $video['Video']['dimensions']?></p>
+<p>Video bitrate: <?php echo $video['Video']['bv']?></p>
+<p>Audio bitrate: <?php echo $video['Video']['ba']?></p>
 <p><?php echo $this->Form->postLink('Delete video', array( 'action' => 'delete', $video['Video']['id']), 
                                     array( 'method' => 'delete') )?></p>
